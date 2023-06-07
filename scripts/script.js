378cd7bc1cc872425799a346bd5db96e6645e5b3
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2023-06-06 23:51:24 -0400

    updated js logic and font and logo

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
 
+console.log(cf_pw);
+
 const element = document.querySelector('.submit');
 
 const inputs = document.querySelectorAll('input');
+
+inputs.forEach(i => {
+    i.addEventListener('keydown', (e) => {
+        console.log(i.id);
+        let x = document.querySelector(`.${i.id}`);
+        document.getElementById(`${x.id}`).innerHTML = '';
+    })
+})
 
 element.addEventListener('click', (e) => {
 
@@ -19,15 +29,7 @@ element.addEventListener('click', (e) => {
     }
 
     if (validate()) {
-        console.log('invalid');
         e.preventDefault();
-        console.log(document.getElementsByClassName('error'));
-        // let x = document.querySelectorAll('.error');
-        // x.forEach(d => {
-        //     document.getElementById(d.id).innerHTML = 
-        // })
-
-
         return false;
     }
 
@@ -40,45 +42,37 @@ element.addEventListener('click', (e) => {
 
 const validate = () => {
 
-    if (first_name.value.length < 5) {
+    if (first_name.value.length === 0) {
         document.getElementById('error_fname').innerHTML = 'Invalid First Name'
         return true;
     };
 
-    if (last_name.value.length < 5) {
+    if (last_name.value.length === 0) {
         console.log('last_name invalid');
         document.getElementById('error_lname').innerHTML = 'Invalid Error'
         return true;
     }
 
-    // if (!email.value) {
-    //     console.log('missing email');
-    //     return true;
+    if (!email.value) {
+        return true;
+    }
 
-    // }
-
-    // if (!phone.value) {
-    //     console.log('missing phone');
-    //     return true;
-
-    // }
+    if (!phone.value) {
+        return true;
+    }
 
     if (password.value.length < 10) {
-        console.log('pw too short');
         document.getElementById('error_pw').innerHTML = 'Invalid Password'
         return true;
     }
 
-    if (password.value.length < 10) {
-        console.log('cf pw too short');
-        document.getElementById('error_cpw').innerHTML = 'Invalid Password'
+    if (cf_pw.value.length < 10) {
+        document.getElementById('error_cpw').innerHTML = 'Invalid Confirm Password'
         return true;
     }
 
     if (password.value !== cf_pw.value) {
-        console.log(password.value);
-        console.log(cf_pw.value);
-        console.log('passwords do not match');
+        alert('Passwords do not match');
         return true;
     }
 
